7b2323f75b9260b1caf6149a43c03815eb20ad9d
Robert Heedt <robert.heedt@gmail.com>
2022-12-04 13:44:58 +0800

    Day 4

---
+++ 04x.py
@@ -0,0 +1,17 @@
+import re
+
+# Regex pattern for (number)-(number),(number)-(number)
+pattern = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
+
+with open("04.txt", "r") as f:
+    count = 0
+
+    while a := f.readline().strip():
+        e1min, e1max, e2min, e2max = pattern.match(a).groups()
+
+        # Test whether the ranges overlap
+        if (int(e1min) <= int(e2min) <= int(e1max) or
+            int(e2min) <= int(e1min) <= int(e2max)):
+            count += 1
+
+    print(count)
