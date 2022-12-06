0f595210edab6976a278e070ed8dd8843ef9b60d
Robert Heedt <robert.heedt@gmail.com>
2022-12-06 14:18:59 +0800

    Day 6: less wasteful iteration

--- 06.py
+++ 06.py
@@ -13,8 +13,10 @@ for i, c in enumerate(signal):
 
     found_dupes = False
 
-    for i1 in range(4):
-        for i2 in range(3):
+    # (0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)
+
+    for i1 in range(3):
+        for i2 in range(i1+1, 4):
             if i1 != i2 and last_four_chars[i1] == last_four_chars[i2]:
                 found_dupes = True
 
--- 06x.py
+++ 06x.py
@@ -13,8 +13,8 @@ for i, c in enumerate(signal):
 
     found_dupes = False
 
-    for i1 in range(14):
-        for i2 in range(13):
+    for i1 in range(13):
+        for i2 in range(i1+1, 14):
             if i1 != i2 and last_fourteen_chars[i1] == last_fourteen_chars[i2]:
                 found_dupes = True
 
